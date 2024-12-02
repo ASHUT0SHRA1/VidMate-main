35ddf2808474b8077c02b35e381eee4cc0b7c81f
Ashutosh Rai <89987124+ASHUT0SHRA1@users.noreply.github.com>
2024-12-02 11:00:34 +0530

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {DB_NAME} from '../constant.js'
+const connectDB = async () => {
+  try {
+    // Connect to the MongoDB cluster
+    const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    console.log("Connected to MongoDB successfully!");
+    console.log(connectionInstance.connection.host)
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
+export default connectDB;
