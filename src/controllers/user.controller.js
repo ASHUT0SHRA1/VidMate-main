da92e408cbd6f8ada55c8f5e36544eb243c6785a
Ashutosh Rai <89987124+ASHUT0SHRA1@users.noreply.github.com>
2024-12-09 10:42:54 +0530

    pipelining donwe

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,8 @@ import {uploadonCloudinary} from '../utils/Cloudinary.js';
 import {ApiResponse} from '../utils/ApiResponse.js'
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { upload } from '../middlewares/multer.middleware.js';
+import mongoose, { mongo } from 'mongoose';
 dotenv.config()
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -205,4 +207,199 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 
 })
-export {registerUser,loginUser, logoutUser , refreshAccessToken}
+
+const changePassword = asyncHandler( async(req , res)=>{
+    const {oldPassword , newPassword} = req.body ; 
+    const user = await User.findById(req.user?._id);
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new ApiError("Invalid password")
+    }
+    user.password = newPassword ; 
+    await user.save({validateBeforeSave : false});
+    return res.status(200).json(
+        new ApiResponse(200 ,{} , "password changes successfully")
+    )
+
+})
+
+const getCurrentUser = asyncHandler(async(req , res)=>{
+    return res.status(200).json(new ApiResponse(200 , req.user , 
+        "current user fetched successfully"
+    ))
+})
+
+const updateAccountDetails = asyncHandler(async(req , res)=>{
+    const {fullname , email } = req.body ; 
+    if(!(fullname || email)){
+        throw new ApiError(400 , "All fields are required")
+    }
+
+    await User.findByIdAndUpdate(
+        req.user?._id, 
+        {
+            $set : {
+                fullname : fullname , 
+                email : email 
+            }
+        }, {
+            new : true
+        }
+    ).select("-password")
+    return res.status(200).json(new ApiResponse(200 , user , "account details update successfully"))
+})
+
+const updateUserAvatar = asyncHandler(async(req , res)=>{
+    const avatarLocalPath =req.file?.path
+    if(!avatarLocalPath){
+        throw new ApiError(400 , "Avatar file is missing")
+    }
+
+    const avatar = await uploadonCloudinary(avatarLocalPath)
+    if(!avatar.url){
+        throw new ApiError(400 , "Error while uploading on avatar")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id , 
+        {
+        $set : avatar.url
+        },
+        {new : true }).select("-password")
+    return res.status(200).json(new ApiResponse(200 , user , "Avatar image updated successfully "))
+})
+
+const updateUserCoverImage = asyncHandler(async(req , res)=>{
+    const coverImagelocalPath =req.file?.path
+    if(!coverImagelocalPath){
+        throw new ApiError(400 , "Avatar file is missing")
+    }
+
+    const cover = await uploadonCloudinary(coverImagelocalPath)
+    if(!cover.url){
+        throw new ApiError(400 , "Error while uploading on avatar")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id , 
+        {
+        $set : cover.url
+        },
+        {new : true }).select("-password")
+    return res.status(200).json(new ApiResponse(200 , user , "Cover image updated successfully "))
+})
+
+const getUserChannelProfile = asyncHandler(async(req , res)=>{
+    const {username}  = req.params
+
+    if(!username.trim()){
+        throw new ApiError(400 , "User inavlid ")
+    }
+
+    const channel =  User.aggregate([
+        {$match : {
+            username : username?.toLowerCase()
+
+        },
+        $lookup : {
+            from :"subscriptions",
+            localField :"_id",
+            foreignField : "channel",
+            as: "subscriber"
+
+        },
+        $lookup : {
+            from :"subscriptions",
+            localField :"_id",
+            foreignField : "subscriber",
+            as: "subscribedTo"
+        },
+        $addFields : {
+            subscribersCount  : {
+                $size : "$subscribers"
+            }, 
+            channelSubscribedTo : {
+                $size : "$subscribedTo"
+            }, 
+            isSubscribed :{
+                $cond : {
+                    if : {$in : [req.user?._id ,  "$subscribers.subscriber"]},
+                    then : true ,
+                    else : false 
+                }
+            }
+        },
+        $project :{
+            fullname : 1 , 
+            username : 1 , 
+            subscribersCount :1  , 
+            channelSubscribedTo : 1 , 
+            isSubscribed : 1 , 
+            avatar : 1 , 
+            coverImage : 1, 
+            email : 1, 
+        }}
+    ])  
+    if(!channel?.length){
+        throw new ApiError(404 , "channel does not exist")
+    }
+
+    return res.status(200).json(new ApiResponse(200 , channel[0] , "User channel fetched successfully"))
+
+})
+
+const getWatchHistory  = asyncHandler(async(req, res)=>{
+    const user = await User.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup : {
+                from : "videos",
+                localField : "watchHistory",
+                foreignField :"_id",
+                as:"watchHistory",
+                pipeline : [
+                    {
+                        $lookup : {
+                            from : "users",
+                            localField :"owner",
+                            foreignField : "_id",
+                            as : "owner",
+                            pipeline : [
+                                {
+                                    $project : {
+                                        fullname : 1 , 
+                                        username : 1 ,
+                                        avatar : 1 
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields : {
+                            owner :{
+                                $first : "$owner"
+                            }
+                        }
+                    }
+                ]
+
+            }
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200 , user[0].watchHistory , "Wathc history fetched successfully"))
+})
+
+export {
+    registerUser,
+    loginUser, 
+    logoutUser , 
+    refreshAccessToken ,
+    getCurrentUser, 
+    changePassword,  
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage , 
+    getUserChannelProfile   , 
+    getWatchHistory  
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from 'express'
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from '../controllers/user.controller.js'
+import {
+    registerUser,
+    loginUser, 
+    logoutUser , 
+    refreshAccessToken ,
+    getCurrentUser, 
+    changePassword,  
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage , 
+    getUserChannelProfile   , 
+    getWatchHistory  
+} 
+from '../controllers/user.controller.js'
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -18,7 +31,15 @@ router.route("/register").post(upload.fields([
 
 router.route("/login" ).post(loginUser);
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT  , changePassword);
+router.route("/current-user").get(verifyJWT , getCurrentUser);
+router.route("/update-account").patch(verifyJWT , updateAccountDetails);
+router.route("/change-avatar").patch(verifyJWT ,upload.single("avatar"),  updateUserAvatar);
+router.route("/change-coverImage").patch(verifyJWT , upload.single("coverImage") , updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile);
+router.route("/history").get(verifyJWT , getWatchHistory);
 //secure route
+
 router.route("/logout").post(verifyJWT ,logoutUser)
 
 
